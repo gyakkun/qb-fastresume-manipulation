5db90fc03defe9dec71bc02d04dd8bb99b0f1c43
gyakkun <xiaowufeixia@gmail.com>
2022-08-19 17:02:46 +0800

    update.

--- src/main/kotlin/Manipulation.kt
+++ src/main/kotlin/Manipulation.kt
@@ -1,21 +1,35 @@
 import Manipulation.FAST_RESUME_EXTENSION
 import Manipulation.bencode
+import Manipulation.getFolder
 import com.dampcake.bencode.Bencode
 import com.dampcake.bencode.Type
 import com.google.common.base.Charsets.UTF_8
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.nio.ByteBuffer
 import java.nio.file.Files
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.streams.asStream
 
 fun main() {
     System.err.println("hello world")
     val logger = LoggerFactory.getLogger(Manipulation.javaClass)
-    Manipulation.getFolder().walk().forEach outer@{
+    val atomicInt = AtomicInteger(0)
+    val resolve = getFolder().resolve("new").apply { this.mkdirs() }
+    getFolder().walk().asStream().parallel().forEach outer@{
         if (it.extension != FAST_RESUME_EXTENSION) return@outer
         runCatching {
             val map = bencode.decode(Files.readAllBytes(it.toPath()), Type.DICTIONARY)
-            logger.info("map {}", map)
+            val byteBuffer = (map["info"]!! as Map<String, Object>)["name"] as ByteBuffer
+            val name = String(byteBuffer.array(), UTF_8)
+            if (name != "[BDMV][210929] ずっと真夜中でいいのに。 - 温れ落ち度") return@outer
+            logger.info("filename {}", it.name)
+            // logger.info("map {}", map)
+            logger.info("torrent name {}", name)
+            val encodeByteArray = bencode.encode(map)
+            Files.write(getFolder().toPath().resolve("new/" + it.name), encodeByteArray)
+            return@outer
         }
             .onFailure { ex -> logger.error("Exception: ", ex) }
     }
